048f74c03473ecdeca9f0d2b4929ac38daa73235
RonStrauss <ashbelthor@gmail.com>
2024-11-02 12:15:42 +0200

    reverseVowels done

--- reverseVowels/main.go
+++ reverseVowels/main.go
@@ -15,13 +15,13 @@ func reverseVowels(s string) string {
 	ptr1, ptr2 := 0, len(s)-1
 	sl := []byte(s)
 
-	for ptr1 != ptr2 && ptr1 > len(s) && ptr2 < 0 {
+	for ptr1 <= ptr2 && ptr1 < len(s) && ptr2 > -1 {
 		c1, c2 := sl[ptr1], sl[ptr2]
 		v1, v2 := strings.Contains(vowels, string(c1)), strings.Contains(vowels, string(c2))
-		if v1 == false {
+		if !v1 {
 			ptr1++
 		}
-		if v2 == false && ptr2 != ptr1 {
+		if !v2 && ptr2 != ptr1 {
 			ptr2--
 		}
 
